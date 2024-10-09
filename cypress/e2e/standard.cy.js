7060b743013041e4633d613d51b224b8acab22af
Anshuman Dash <anshuman@ucsb.edu>
2024-10-09 12:04:51 -0700

    First cypress test added

--- cypress/e2e/standard.cy.js
+++ cypress/e2e/standard.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
-  it('passes', () => {
+  // Beforeall it should go to localhost:5173
+  before(() => {
     cy.visit('localhost:5173/')
+  })
+
+  it('Has the right title' , () => {
+    // Check that the url title is "Anshuman Dash"
+    cy.title().should('eq', 'Anshuman Dash')
   })
 })
