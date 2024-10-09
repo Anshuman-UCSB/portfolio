5bc888f01c3c6cc88c232e6a535484416013f32a
Anshuman Dash <anshuman@ucsb.edu>
2024-10-09 14:02:18 -0700

    Adding in corners and background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import "./App.css";
+import topLeft from "./assets/top left.svg";
+import bottomRight from "./assets/bottom right.svg";
+
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="bg-cream w-screen h-screen p-12 relative">
+      <div className="p-12 relative w-full h-full">
+        <img
+          src={topLeft}
+          alt="Top left svg"
+          className="absolute top-0 left-0 w-16 h-16"
+        />
+        <img
+          src={bottomRight}
+          alt="Bottom right svg"
+          className="absolute bottom-0 right-0 w-16 h-16"
+        />
+        <h1>Hello world</h1>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: '#FAECD2',
+      },
+    },
   },
   plugins: [],
 }
