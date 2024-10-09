24989be7d0526e6ee2717981741fae04b18eaf91
Anshuman Dash <anshuman@ucsb.edu>
2024-10-09 15:20:25 -0700

    Combining headers

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -1,4 +1,24 @@
-import Header from "./Header";
+import { useSelected } from "../SelectedContext";
+
+interface HeaderProps {
+  name: string;
+}
+
+function Header({ name }: HeaderProps) {
+  const { selected, setSelected } = useSelected();
+
+  return (
+    <p
+      className={`inline-block transition-all duration-500 ease-in-out font-semibold text-4xl ${
+        selected === name ? "text-darkbrown" : "text-faded"
+      }`}
+      onClick={() => setSelected(name)}
+      style={{ width: "auto" }}
+    >
+      {name}
+    </p>
+  );
+}
 
 function Headers() {
   return (
