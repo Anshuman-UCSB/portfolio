052f13b0f97826e00a273c8502eb26b51b3b85a9
Anshuman Dash <anshuman@ucsb.edu>
2024-10-10 11:39:53 -0700

    Update tests

--- cypress/e2e/standard.cy.js
+++ cypress/e2e/standard.cy.js
@@ -1,27 +1,33 @@
-describe('template spec', () => {
+describe("template spec", () => {
   // Beforeall it should go to localhost:5173
   beforeEach(() => {
-    cy.visit('localhost:5173/')
-  })
+    cy.visit("localhost:5173/");
+  });
 
-  it('Has the right title' , () => {
+  it("Has the right title", () => {
     // Check that the url title is "Anshuman Dash"
-    cy.title().should('eq', 'Anshuman Dash')
-  })
+    cy.title().should("eq", "Anshuman Dash");
+  });
 
-  it('Has selectable headers', ()=>{
-    /* ==== Generated with Cypress Studio ==== */
-    cy.get('.font-bold').should('have.text', 'Anshuman Dash');
-    cy.get('.px-6 > .text-darkbrown').should('have.text', 'ABOUT');
-    cy.get('.px-6 > .text-darkbrown').should('have.class', 'text-4xl');
-    cy.get('.px-6 > .text-darkbrown').should('have.class', 'text-darkbrown');
-    cy.get('.px-6 > :nth-child(2)').should('have.text', 'PROJECTS');
-    cy.get('.px-6 > :nth-child(3)').should('have.text', 'CONTACT');
-    cy.get('.px-6 > :nth-child(2)').should('have.class', 'text-faded');
-    cy.get('.px-6 > :nth-child(3)').should('have.class', 'text-faded');
-    cy.get('.px-6 > :nth-child(3)').click();
-    cy.get('.px-6 > .text-darkbrown').should('have.class', 'text-darkbrown');
-    cy.get('.px-6 > :nth-child(1)').should('have.class', 'text-faded');
-    /* ==== End Cypress Studio ==== */
-  })
-})
+  it("Has selectable headers", () => {
+    cy.get(".font-bold").should("have.text", "Anshuman Dash");
+    cy.get(":nth-child(1) > .cursor-pointer").should("have.text", "ABOUT");
+    cy.get(":nth-child(2) > .cursor-pointer").should(
+      "have.class",
+      "text-faded"
+    );
+    cy.get(":nth-child(1) > .cursor-pointer").should(
+      "have.class",
+      "text-darkbrown"
+    );
+    cy.get(":nth-child(2) > .cursor-pointer").click();
+    cy.get(":nth-child(2) > .cursor-pointer").should(
+      "have.class",
+      "text-darkbrown"
+    );
+    cy.get(":nth-child(1) > .cursor-pointer").should(
+      "have.class",
+      "text-faded"
+    );
+  });
+});
