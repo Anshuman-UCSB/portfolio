683d410154b7e4334ce7d5354b8b503f1a2635e2
Anshuman Dash <anshuman@ucsb.edu>
2024-10-10 11:06:23 -0700

    Make headers look clickable

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -9,7 +9,7 @@ function Header({ name }: HeaderProps) {
 
   return (
     <p
-      className={`inline-block transition-all duration-500 ease-in-out font-semibold text-4xl ${
+      className={`cursor-pointer inline-block transition-all duration-500 ease-in-out font-semibold text-4xl ${
         selected === name ? "text-darkbrown" : "text-faded"
       }`}
       onClick={() => setSelected(name)}
